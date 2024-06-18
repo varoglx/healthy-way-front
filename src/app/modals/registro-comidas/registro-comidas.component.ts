8f819bc064599c212afa3dc15fead1f3828a062e
varoglx <114885048+varoglx@users.noreply.github.com>
2024-06-18 06:18:56 -0400

    a
    a

--- src/app/modals/registro-comidas/registro-comidas.component.ts
+++ src/app/modals/registro-comidas/registro-comidas.component.ts
@@ -5,7 +5,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { HttpClient } from '@angular/common/http';
 import { RegistrarIngestaComponent } from '../registrar-ingesta/registrar-ingesta.component';
-
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-registro-comidas',
   templateUrl: './registro-comidas.component.html',
@@ -15,15 +15,17 @@ export class RegistroComidasComponent implements OnInit, OnDestroy {
 
   ingestaForm: FormGroup;
   ingestaHoy: any[] = [];
+  totalCalorias: number = 0;
   private intervalId: any;
-
+  usuario: any;
   constructor(
     private modalController: ModalController,
     private formBuilder: FormBuilder,
     private firestore: AngularFirestore,
     private storage: AngularFireStorage,
     private alertController: AlertController,
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService,
   ) {
     this.ingestaForm = this.formBuilder.group({
       nombre_receta: ['', Validators.required],
@@ -42,8 +44,14 @@ export class RegistroComidasComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.loadUsuario();
     this.loadIngestaHoy();
     this.startPolling();
+  }
+  loadUsuario() {
+    const usuarioStr = localStorage.getItem('usuario');
+    this.usuario = usuarioStr ? JSON.parse(usuarioStr) : null;
+    console.log(this.usuario.uid);
   }
 
   ngOnDestroy() {
@@ -53,6 +61,7 @@ export class RegistroComidasComponent implements OnInit, OnDestroy {
   startPolling() {
     this.intervalId = setInterval(() => {
       this.loadIngestaHoy();
+      this.calculateTotalCalorias();
     }, 1000); // Check every second
   }
 
@@ -65,7 +74,27 @@ export class RegistroComidasComponent implements OnInit, OnDestroy {
   loadIngestaHoy() {
     const ingestaHoyStr = localStorage.getItem('ingestaHoy');
     this.ingestaHoy = ingestaHoyStr ? JSON.parse(ingestaHoyStr) : [];
-    console.log(this.ingestaHoy)
   }
+
+  calculateTotalCalorias() {
+    this.totalCalorias = this.ingestaHoy.reduce((total, comida) => total + comida.calorias, 0);
+  }
+
+  async enviarTotalCalorias() {
+    this.authService.getCurrentUser().subscribe(async user => {
+      if (user) {
+        const userUid = user.uid;
+        const fechaHoy = new Date().toISOString().split('T')[0]; // Obtiene la fecha en formato YYYY-MM-DD
+  
+        await this.firestore.collection(`users/${userUid}/calorias`).add({
+          fecha: fechaHoy,
+          totalCalorias: this.totalCalorias
+        });
+        localStorage.setItem('ingestaHoy', JSON.stringify([]));
+        console.log(`Total de Calorías Hoy: ${this.totalCalorias}`);
+      }
+    });
+  }
+  
 
 }
